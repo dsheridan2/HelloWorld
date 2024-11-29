42c3d1889ba19cc6cdbd4b7bfb578ba1cdef029c
David Sheridan <dsheridan2@gmail.com>
2024-11-29 12:27:25 -0500

    Final Project Start

---
+++ ZoneCreator/src/ZoneCreator.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ZoneCreator extends Application{
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World!");
+        primaryStage.show();
+    }
+}
